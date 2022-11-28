50686fbdf3d474786d7810575e468497fcb153ce
HarshaSalih <harshasalih2000@gmail.com>
2022-11-28 22:52:36 +0530

    commits

--- src/app/student-entry/student-entry.component.ts
+++ src/app/student-entry/student-entry.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class StudentEntryComponent {
 
+  name=""
+  rollNo=""
+  admissionNo=""
+  mobile=""
+  college=""
+  parentName=""
+  parentNo=""
+  userName=""
+  password=""
+  confirmPassword=""
+
+
+  readValues=()=>
+  {
+    let data:any={"name":this.name,"rollNo":this.rollNo,"admissionNo":this.admissionNo,"mobile":this.mobile,"college":this.college,"parentName":this.parentName,"parentNo":this.parentNo,"userName":this.userName,"password":this.password,"confirmPassword":this.confirmPassword}
+    console.log(data)
+  }
+
+
 }
